3cb433b2c5ce74f690e5b59f38516f357926c671
Jaime Iserte <jiserte@differentroads.es>
2025-02-04 09:58:52 +0100

    Ordenamos AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+// Angular Core Imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+// App Routing and Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Layout Components
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { MainComponent } from './layout/main/main.component';
 
-//primeng
-import { MenubarModule } from 'primeng/menubar';
+// Page Components
+import { HomeComponent } from './pages/home/home.component';
+import { HeroSectionComponent } from './pages/home/components/hero-section/hero-section.component';
+import { ToursSectionComponent } from './pages/home/components/tours-section/tours-section.component';
 
+// Community Components
+import { CommunitySectionComponent } from './pages/home/components/community-section/community-section.component';
+import { CommunityHeroComponent } from './pages/home/components/community-section/components/community-hero/community-hero.component';
+import { CommunityGalleryComponent } from './pages/home/components/community-section/components/community-gallery/community-gallery.component';
+import { CommunityReviewsComponent } from './pages/home/components/community-section/components/community-reviews/community-reviews.component';
+
+// PrimeNG Configuration
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+// PrimeNG Modules
+import { MenubarModule } from 'primeng/menubar';
 import { TabsModule } from 'primeng/tabs';
 import { EditorModule } from 'primeng/editor';
 import { FloatLabelModule } from 'primeng/floatlabel';
@@ -18,14 +40,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ToggleButtonModule } from 'primeng/togglebutton';
 import { FileUploadModule } from 'primeng/fileupload';
 import { ImageModule } from 'primeng/image';
-
-import { ApplicationConfig } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 import { RatingModule } from 'primeng/rating';
-import { provideHttpClient } from '@angular/common/http';
-
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
 import { AutoCompleteModule } from 'primeng/autocomplete';
@@ -33,35 +48,38 @@ import { CalendarModule } from 'primeng/calendar';
 import { ButtonModule } from 'primeng/button';
 import { CarouselModule } from 'primeng/carousel';
 import { CardModule } from 'primeng/card';
-import { HomeComponent } from './pages/home/home.component';
-import { FormsModule } from '@angular/forms';
 import { DatePickerModule } from 'primeng/datepicker';
-import { HeroSectionComponent } from './pages/home/components/hero-section/hero-section.component';
-import { ToursSectionComponent } from './pages/home/components/tours-section/tours-section.component';
-import { CommunitySectionComponent } from './pages/home/components/community-section/community-section.component';
 import { RippleModule } from 'primeng/ripple';
-import { CommunityHeroComponent } from './pages/home/components/community-section/components/community-hero/community-hero.component';
-import { CommunityGalleryComponent } from './pages/home/components/community-section/components/community-gallery/community-gallery.component';
-import { CommunityReviewsComponent } from './pages/home/components/community-section/components/community-reviews/community-reviews.component';
 
 @NgModule({
   declarations: [
+    // App Components
     AppComponent,
+
+    // Layout Components
     HeaderComponent,
     FooterComponent,
     MainComponent,
+
+    // Page Components
     HomeComponent,
     HeroSectionComponent,
     ToursSectionComponent,
+
+    // Community Components
     CommunitySectionComponent,
     CommunityHeroComponent,
     CommunityGalleryComponent,
-    CommunityReviewsComponent
+    CommunityReviewsComponent,
   ],
   imports: [
+    // Angular Modules
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    FormsModule,
+
+    // PrimeNG Modules
     MenubarModule,
     RippleModule,
     AvatarModule,
@@ -72,7 +90,6 @@ import { CommunityReviewsComponent } from './pages/home/components/community-sec
     ButtonModule,
     CarouselModule,
     CardModule,
-    FormsModule,
     DatePickerModule,
     ImageModule,
     RatingModule,
@@ -87,12 +104,12 @@ import { CommunityReviewsComponent } from './pages/home/components/community-sec
         options: {
           cssLayer: {
             name: 'primeng',
-            order: 'tailwind-base, primeng, tailwind-utilities'
+            order: 'tailwind-base, primeng, tailwind-utilities',
           },
-        }
-      }
-    })
+        },
+      },
+    }),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

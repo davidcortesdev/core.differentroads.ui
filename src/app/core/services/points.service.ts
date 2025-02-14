00d6c05c7c5ad04b89b28dd4df3dd7ed573e6684
Luis Monsalve <luisem0011@gmail.com>
2025-02-14 16:57:08 -0500

    feat: add points model and service for managing user points data

---
+++ src/app/core/services/points.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import {
+  Point,
+  GetAllPointsParams,
+  PointListResponse,
+} from '../models/points/point.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PointsService {
+  private readonly API_URL = `${environment.dataApiUrl}/points`;
+  private readonly httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  createPoints(point: Point): Observable<Point> {
+    return this.http.post<Point>(this.API_URL, point, this.httpOptions);
+  }
+
+  getPoints(id: string): Observable<Point> {
+    return this.http.get<Point>(`${this.API_URL}/${id}`, this.httpOptions);
+  }
+
+  getAllPoints(params?: GetAllPointsParams): Observable<PointListResponse> {
+    const httpParams = params
+      ? new HttpParams({ fromObject: params as any })
+      : undefined;
+    return this.http.get<PointListResponse>(this.API_URL, {
+      params: httpParams,
+      ...this.httpOptions,
+    });
+  }
+
+  getPointsByDni(
+    dni: string,
+    params?: GetAllPointsParams
+  ): Observable<PointListResponse> {
+    const httpParams = params
+      ? new HttpParams({ fromObject: params as any })
+      : undefined;
+    return this.http.get<PointListResponse>(`${this.API_URL}/by-user/${dni}`, {
+      params: httpParams,
+      ...this.httpOptions,
+    });
+  }
+
+  getTotalPointsByDni(dni: string): Observable<number> {
+    return this.http.get<number>(
+      `${this.API_URL}/by-user/${dni}/total-points`,
+      this.httpOptions
+    );
+  }
+}

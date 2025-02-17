7f2408135df47bd305e28e5934e2d995550f60f9
Jaime Iserte <jiserte@differentroads.es>
2025-02-17 09:47:13 +0100

    ajustamos en la posición que tiene que estar y para que no afecte

--- src/app/pages/home/components/full-card-section/full-card-section.component.ts
+++ src/app/pages/home/components/full-card-section/full-card-section.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { BlockType } from '../../../../core/models/blocks/block.model';
+import { FullSliderContent } from '../../../../core/models/blocks/full-slider-content.model';
 
 type Card = {
   id: number;
-  title: string;
+  title?: string;
   description: string;
   image: { url: string; alt: string };
 };
@@ -15,22 +17,25 @@ type Card = {
   styleUrls: ['./full-card-section.component.scss'],
 })
 export class FullCardSectionComponent implements OnInit {
-  @Input() cards: Card[] = [
+  @Input() content!: Card[];
+  @Input() type!: BlockType;
+  @Input() title!: string;
+  cards: Card[] = [
     {
       id: 1,
-      title: 'Descubriendo monasterios en la cima del mundo',
+      title: 'aaaDescubriendo monasterios en la cima del mundo',
       description: 'Armenia y Georgia en: 12 noches',
       image: { url: 'https://picsum.photos/1000?random=20', alt: 'Image 1' },
     },
     {
       id: 2,
-      title: 'Siguiendo el legado de la ruta de la seda',
+      title: 'aaaaSiguiendo el legado de la ruta de la seda',
       description: 'Uzbekistán en: 10 días ',
       image: { url: 'https://picsum.photos/1000?random=15', alt: 'Image 2' },
     },
     {
       id: 3,
-      title: 'Primavera sakura en la ciudad del sol naciente',
+      title: 'aaaaaPrimavera sakura en la ciudad del sol naciente',
       description: 'Japón en: 11 días ',
       image: { url: 'https://picsum.photos/1000?random=16', alt: 'Image 3' },
     },
@@ -38,8 +43,12 @@ export class FullCardSectionComponent implements OnInit {
 
   constructor(private sanitizer: DomSanitizer) {}
   ngOnInit(): void {
+    console.log('this.content', this.content);
+
     if (!this.cards || this.cards.length !== 3) {
-      console.error('Cards input is undefined or does not contain exactly 3 cards');
+      console.error(
+        'Cards input is undefined or does not contain exactly 3 cards'
+      );
     }
   }
 

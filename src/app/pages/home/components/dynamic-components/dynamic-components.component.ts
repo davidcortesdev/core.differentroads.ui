8c6248f8565d0714343d61364d5145c2321e7695
Jaime Iserte <jiserte@differentroads.es>
2025-02-25 13:09:05 +0100

    ajustamos para llamada async a los blocks

--- src/app/pages/home/components/dynamic-components/dynamic-components.component.ts
+++ src/app/pages/home/components/dynamic-components/dynamic-components.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input, Type } from '@angular/core';
-import { HomeService } from '../../../../core/services/home.service';
+import { Component, Input, Type } from '@angular/core';
 import { Block, BlockType } from '../../../../core/models/blocks/block.model';
 import { HighlightSectionComponent } from '../highlight-section/highlight-section.component';
 import { ContentListComponent } from '../content-list/content-list-section.component';
@@ -14,24 +13,8 @@ import { CarouselSectionComponent } from '../carousel-section/carousel-section.c
   templateUrl: './dynamic-components.component.html',
   styleUrls: ['./dynamic-components.component.scss'],
 })
-export class DynamicComponentsComponent implements OnInit {
+export class DynamicComponentsComponent {
   @Input() blocks: Block[] = [];
-
-  constructor(private homeService: HomeService) {} // Removed injector since we won't use it
-
-  ngOnInit(): void {
-    if (this.blocks.length === 0) {
-      this.homeService.getDynamicSections().subscribe({
-        next: (data: Block[]) => {
-          this.blocks = data;
-        },
-        error: (error: any) => {
-          console.error('Error fetching home data:', error);
-        },
-      });
-    }
-   
-  }
 
   getComponent(block: Block): Type<any> | null {
     switch (block.type) {

21130528af2139a9d8f82aa04f887a60e615f10b
Jaime Iserte <jiserte@differentroads.es>
2025-02-28 16:14:56 +0100

    optimizado

--- src/app/pages/home/components/carousel-section/carousel-section.component.ts
+++ src/app/pages/home/components/carousel-section/carousel-section.component.ts
@@ -3,103 +3,68 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { BlockType } from '../../../../core/models/blocks/block.model';
 import { FullSliderContent } from '../../../../core/models/blocks/full-slider-content.model';
 
-type Card = {
+interface Card {
   id: number;
   description: string;
-  image: { url: string; alt: string };
+  image: {
+    url: string;
+    alt: string;
+  };
   link: string;
-};
+}
+
+interface ResponsiveOption {
+  breakpoint: string;
+  numVisible: number;
+  numScroll: number;
+}
 
 @Component({
   selector: 'app-carousel-section',
   standalone: false,
   templateUrl: './carousel-section.component.html',
-  styleUrls: ['./carousel-section.component.scss'],
+  styleUrls: ['./carousel-section.component.scss']
 })
 export class CarouselSectionComponent implements OnInit {
   @Input() content!: FullSliderContent;
   @Input() type!: BlockType;
   @Input() title!: string;
-  titlegeneral: string = ''; 
-  descriptiongeneral: string = ''; 
+
   cards: Card[] = [];
-  textoquill: string='';
+  textoquill = '';
+  responsiveOptions: ResponsiveOption[] = [
+    { breakpoint: '1024px', numVisible: 3, numScroll: 1 },
+    { breakpoint: '768px', numVisible: 2, numScroll: 1 },
+    { breakpoint: '560px', numVisible: 1, numScroll: 1 }
+  ];
 
-  responsiveOptions: { breakpoint: string; numVisible: number; numScroll: number }[] = [];
-
-  constructor(private sanitizer: DomSanitizer, private cdr: ChangeDetectorRef) {}
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
-    console.log('this.content', this.content);
-    if (this.type === BlockType.CardSliderVertical && this.content && this.content['card-list']) {
+    if (this.isValidContent()) {
       this.textoquill = this.content.content;
-
-      
-      this.cards = this.content['card-list'].map((card: any, index: number) => {
-        
-          return {
-            id: index + 1,
-            description: card.description,
-            image: {
-              url: card.image[0].url,
-              alt: `Image ${index + 1}`,
-            },
-            link: card.link || '#',
-          };
-        
-      });
+      this.initializeCards();
     } else {
       console.error('No cards received or cards array is empty');
     }
-
-    this.responsiveOptions = [
-      {
-        breakpoint: '1024px',
-        numVisible: 3,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '768px',
-        numVisible: 2,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '560px',
-        numVisible: 1,
-        numScroll: 1,
-      },
-    ];
   }
 
-  extractTitleAndDescriptionFromHtml(html: string): { title: string, description: string } {
-    const div = document.createElement('div');
-    div.innerHTML = html;
-
-    const titleElement = div.querySelector('em');
-    const descriptionElement = div.querySelector('span');
-
-    const title = titleElement?.textContent || '';
-    const description = descriptionElement?.textContent || '';
-
-    return { title, description };
+  private isValidContent(): boolean {
+    return this.type === BlockType.CardSliderVertical 
+      && this.content 
+      && Array.isArray(this.content['card-list']);
   }
 
-  extractTitleAndDescriptionFromHtmlGeneral(html: string): { titlegeneral: string, descriptiongeneral: string } {
-    const div = document.createElement('div');
-    div.innerHTML = html;
-
-    const titleElement = div.querySelector('strong');
-    const titlegeneral = titleElement?.textContent || '';
-
-
-    const descriptionElement = div.querySelector('span');
-    const descriptiongeneral = descriptionElement?.textContent || '';
-
-    return { titlegeneral, descriptiongeneral };
-  }
-
-  getSanitizedDescription(description: string): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(description);
+  private initializeCards(): void {
+    this.cards = this.content['card-list'].map((card: any, index: number) => ({
+      id: index + 1,
+      description: card.description,
+      image: {
+        url: card.image[0].url,
+        alt: `Image ${index + 1}`
+      },
+      link: card.link || '#'
+    }));
   }
 
   sanitizeHtml(html: string): SafeHtml {

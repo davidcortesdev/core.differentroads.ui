23283d8dceb689760d747bf85c826d6867d54e32
Jaime Iserte <jiserte@differentroads.es>
2025-07-22 17:09:37 +0200

    feat(flight-management): agregar lógica para activar búsqueda específica de vuelos
    - Se implementó la lógica en el componente `FlightManagementComponent` para determinar si la búsqueda específica de vuelos debe estar activa, basándose en la respuesta del servicio de salidas.
    - Se actualizó la plantilla HTML para mostrar la sección de búsqueda específica solo cuando `isConsolidadorVuelosActive` es verdadero, mejorando la experiencia del usuario al gestionar vuelos.

--- src/app/pages/checkout-v2/components/flight-management/flight-management.component.ts
+++ src/app/pages/checkout-v2/components/flight-management/flight-management.component.ts
@@ -1,13 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DepartureService, IDepartureResponse } from '../../../../core/services/departure/departure.service';
 
 @Component({
   selector: 'app-flight-management',
   standalone: false,
-  
+
   templateUrl: './flight-management.component.html',
-  styleUrl: './flight-management.component.scss'
+  styleUrls: ['./flight-management.component.scss']
 })
-export class FlightManagementComponent {
-  @Input() departureId: number | null = null;
-  @Input() reservationId: number | null = null;
+export class FlightManagementComponent implements OnInit {
+  @Input() departureId!: number;
+  @Input() reservationId!: number;
+
+  isConsolidadorVuelosActive: boolean = false;
+
+  constructor(private departureService: DepartureService,
+  ) { }
+
+  ngOnInit(): void {
+    if (this.departureId) {
+      this.departureService.getById(this.departureId).subscribe({
+        next: (departure: IDepartureResponse) => {
+          this.isConsolidadorVuelosActive = !!departure.isConsolidadorVuelosActive;
+        },
+        error: () => {
+          this.isConsolidadorVuelosActive = false;
+        }
+      });
+    }
+  }
 }

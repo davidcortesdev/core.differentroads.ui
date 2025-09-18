dc97da1fc6a5f834614bb776f5e39b93b0854ed1
adriDifferentRoads <agimeno@differentroads.es>
2025-09-18 12:48:15 +0200

    Merge branch 'develop' into main to synchronize changes

---
+++ src/app/core/services/reservationFlight.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { IAgeGroupPriceDTO, IFlightResponse } from "./flight-search.service";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
+
+export interface IFlightPackDTO {
+    id: number;
+    code?: string | null;
+    name?: string | null;
+    description?: string | null;
+    tkId?: string | null;
+    itineraryId: number;
+    isOptional: boolean;
+    imageUrl?: string | null;
+    imageAlt?: string | null;
+    isVisibleOnWeb: boolean;
+    ageGroupPrices?: IAgeGroupPriceDTO[] | null;
+    flights?: IFlightResponse[] | null;
+  }
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ReservationFlightService {
+    private readonly API_URL = `${environment.reservationsApiUrl}/ReservationFlightPacks`;
+
+    constructor(private http: HttpClient) {}
+  
+    getSelectedFlightPack(reservationId: number): Observable<IFlightPackDTO[]> {
+        const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        });
+        
+        return this.http.get<IFlightPackDTO[]>(`${this.API_URL}/${reservationId}`, { headers })
+            .pipe(
+                catchError(error => {
+                    console.error('Error al obtener flight pack:', error);
+                    return throwError(() => error);
+                })
+            );
+    }
+}

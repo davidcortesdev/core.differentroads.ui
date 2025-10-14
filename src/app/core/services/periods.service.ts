d733f1229c591f142fea9855630913872e7ceb8f
Jaime Iserte <jiserte@differentroads.es>
2025-10-14 10:53:03 +0200

    refactor(services): clean up PeriodsService by removing unused methods and imports
    - Deleted unused methods and imports from PeriodsService to streamline the service functionality.
    - This cleanup enhances maintainability and reduces complexity in the application by eliminating unnecessary code.

--- src/app/core/services/periods.service.ts
+++ src/app/core/services/periods.service.ts
@@ -1,44 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, Observable, catchError, switchMap, tap, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { Activity } from '../models/tours/activity.model';
-import { Insurance } from '../models/tours/insurance.model';
-import { Period } from '../models/tours/period.model';
-import { ReservationMode } from '../models/tours/reservation-mode.model';
-import { Flight } from '../models/tours/flight.model';
-import { PriceData } from '../models/commons/price-data.model';
-import { TourNetService } from './tourNet.service';
 
-type SelectedFields = Partial<Array<keyof Period | 'all'>>;
 
 @Injectable({
   providedIn: 'root',
 })
 export class PeriodsService {
-  private readonly DATA_API_URL = `${environment.dataApiUrl}/periods`;
 
   constructor(
     private http: HttpClient,
-    private tourNetService: TourNetService
   ) {}
-
-  /**
-   * Fetches the prices of a period by its external ID.
-   * @param id - The external ID of the period.
-   * @returns Observable of PriceData array. List of prices for activities, tour,
-   * period and flights. the key is the external ID of each component.
-   */
-  getPeriodPrices(id: string): Observable<{
-    [key: string]: { priceData: PriceData[]; availability?: number };
-  }> {
-    return this.http
-      .get<{
-        [key: string]: { priceData: PriceData[]; availability?: number };
-      }>(`${this.DATA_API_URL}/${id}/prices`)
-      .pipe(map((response) => response || []));
-  }
-
 
 /**
  * Obtiene toda la información de la departure asociada a un periodo usando el tkid.

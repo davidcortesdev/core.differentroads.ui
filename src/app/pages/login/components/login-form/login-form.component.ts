9119193d372ec487ac19f3f5c6ae2784c591f5eb
David Cortés <dcortes@differentroads.es>
2025-11-17 16:59:53 +0100

    feat(user-model): add new fields for user creation and login source
    - Introduced 'politicasAceptadas' and 'detalleDeLaFuenteDeRegistro1' fields to the UserCreate interface.
    - Updated the LoginFormComponent to include 'detalleDeLaFuenteDeRegistro1' with a default value during user creation.

--- src/app/pages/login/components/login-form/login-form.component.ts
+++ src/app/pages/login/components/login-form/login-form.component.ts
@@ -380,6 +380,7 @@ export class LoginFormComponent implements OnInit {
       phone: undefined, // Teléfono por defecto
       hasWebAccess: true,
       hasMiddleAccess: false,
+      detalleDeLaFuenteDeRegistro1: 'Formulario de inicio de sesión'
     };
 
     console.log('🚀 Enviando petición de creación...');

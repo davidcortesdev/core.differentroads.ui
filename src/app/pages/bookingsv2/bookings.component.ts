20372fa1f31a751c621b52af69164aee8da35185
David Cortés <dcortes@differentroads.es>
2025-12-11 15:54:38 +0100

    fix: Refine Tour Operator detection logic in query parameters
    - Updated the handling of `isTO` to ensure it correctly interprets the query parameter, maintaining compatibility with `isTourOperator`.
    - Default value for `isTO` is now set to false if neither parameter is provided.

--- src/app/pages/bookingsv2/bookings.component.ts
+++ src/app/pages/bookingsv2/bookings.component.ts
@@ -272,8 +272,17 @@ export class Bookingsv2Component implements OnInit, OnDestroy {
     this.route.queryParams.subscribe((queryParams) => {
       this.isATC = queryParams['isATC'] === 'true';
       
-      // Detectar isTO desde query params: si viene isTO=true o isTourOperator=true, deshabilitar botón
-      this.isTO = queryParams['isTO'] === 'true' || queryParams['isTourOperator'] === 'true';
+      // Detectar isTO desde query params enviado por touroperación
+      // Acepta: ?isTO=true o ?isTO=false
+      // También mantiene compatibilidad con isTourOperator
+      if (queryParams['isTO'] !== undefined) {
+        this.isTO = queryParams['isTO'] === 'true';
+      } else if (queryParams['isTourOperator'] !== undefined) {
+        this.isTO = queryParams['isTourOperator'] === 'true';
+      } else {
+        // Si no viene el parámetro, mantener el valor por defecto (false)
+        this.isTO = false;
+      }
 
       // Actualizar validaciones del formulario según si es ATC
       const cancelationFeeControl = this.cancelForm.get('cancelationFee');

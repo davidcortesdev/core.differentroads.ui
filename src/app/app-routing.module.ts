643dbfbab4843b6c3eae5fec1b71fc3b7547e338
David Cortés <dcortes@differentroads.es>
2026-01-08 13:07:51 +0100

    Add NewReservationComponent routes: Introduce new routes for NewReservationComponent in app-routing.module.ts to handle reservation views with and without paymentId, enhancing booking functionality.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,7 @@ import { TourV2Component } from './pages/tour-v2/tour-v2.component';
 // ========================================
 import { CheckoutV2Component } from './pages/checkout-v2/checkout-v2.component';
 import { ReservationInfoComponent } from './pages/checkout-v2/components/reservation-info/reservation-info.component';
+import { NewReservationComponent } from './pages/checkout-v2/components/new-reservation/new-reservation.component';
 
 // ========================================
 // PAGE COMPONENTS - BOOKINGS
@@ -79,6 +80,15 @@ const routes: Routes = [
       {
         path: 'reservation/:reservationId',
         component: ReservationInfoComponent,
+      },
+      // Rutas del componente HIJO (sin purchase event)
+      {
+        path: 'reservation-view/:reservationId/:paymentId',
+        component: NewReservationComponent,
+      },
+      {
+        path: 'reservation-view/:reservationId',
+        component: NewReservationComponent,
       },
       { path: 'forget-password', component: ForgetPasswordComponent },
       // ========================================
@@ -138,6 +148,15 @@ const routes: Routes = [
       {
         path: 'reservation/:reservationId',
         component: ReservationInfoComponent,
+      },
+      // Rutas del componente HIJO (sin purchase event)
+      {
+        path: 'reservation-view/:reservationId/:paymentId',
+        component: NewReservationComponent,
+      },
+      {
+        path: 'reservation-view/:reservationId',
+        component: NewReservationComponent,
       },
 
       // ========================================

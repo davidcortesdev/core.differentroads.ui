dc21a940f8e86fe44951d3f956e5b1d9456eda68
David Cortés <dcortes@differentroads.es>
2026-01-28 10:05:37 +0100

    feat(app-routing): add HomeV3Component route
    - Introduced a new route for HomeV3Component, enhancing the application's navigation structure.

---
+++ src/app/pages/home-v3/home-v3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home-v3',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-v3.component.html',
+  styleUrls: ['./home-v3.component.scss'],
+})
+export class HomeV3Component {}
+

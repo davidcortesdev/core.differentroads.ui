235156eadeb7d499137dbba2c67990b1d21d06b9
David Cortés <dcortes@differentroads.es>
2026-01-28 11:21:28 +0100

    feat(home-v3): implement header component in HomeV3
    - Added <app-home-v3-header> to the HomeV3 component template for improved structure.
    - Updated imports in HomeV3Component to include HomeV3HeaderComponent, enhancing modularity.

--- src/app/pages/home-v3/home-v3.component.ts
+++ src/app/pages/home-v3/home-v3.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeV3HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-home-v3',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HomeV3HeaderComponent],
   templateUrl: './home-v3.component.html',
   styleUrls: ['./home-v3.component.scss'],
 })
